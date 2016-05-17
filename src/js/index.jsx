6f28a84977c95bbd1e340cbc5ed8c9e8e9a43058
yan.zhang <yan.zhang@yunshipei.com>
2016-05-17 19:23:13 +0800

    no message

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,28 +4,59 @@
 
 
 // 在这里引用less会打包、编译合并到dist/css中
-import style from './components/comment/css/comment.less';
-import style2 from './components/hello/css/hello.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentBoxComponent from './components/comment/js/CommentBoxComponent';
-import HelloComponent from './components/hello/js/HelloComponent';
+import SelectComponent from './components/select/js/SelectComponent';
+import TableComponent from './components/table/js/TableComponent';
 
-var commentData = [
-    {author: "张三1", time: "20150721", text: "张三说好"},
-    {author: "李四1", time: "20150721", text: "李四说好"}
+
+const selectData = [
+    {"id":"jack", "name":"杰克1"},
+    {"id":"lucy", "name":"露西2"},
+    {"id":"tom" , "name":"汤姆3"}
 ];
 
-ReactDOM.render(
-<CommentBoxComponent commentItems={commentData} />,
-    document.body
-);
-//
-//ReactDOM.render(
-//    <HelloComponent />,
-//    document.body
-//);
+const tableColumns = [{
+        title: '姓名',
+        dataIndex: 'name',
+        render: text => <a href="#">{text}</a>
+    }, {
+        title: '年龄',
+        dataIndex: 'age'
+    }, {
+        title: '住址',
+        dataIndex: 'address'
+}];
+const tableData = [{
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号'
+    }, {
+        key: '2',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园2号'
+    }, {
+        key: '3',
+        name: '李大嘴',
+        age: 32,
+        address: '西湖区湖底公园3号'
+}];
 
+
+function selected(v){
+    console.info(v);
+    tableColumns[0].title = "aaa";
+}
+
+
+ReactDOM.render(
+    <div>
+        <SelectComponent data={selectData} selected={selected} />
+        <TableComponent tableColumns={tableColumns} tableData={tableData} />
+    </div>
+    , document.body);
 
 
 
---
+++ src/js/index_bak.jsx
@@ -0,0 +1,36 @@
+/**
+ * Created by a1 on 2016/5/5.
+ */
+
+
+// 在这里引用less会打包、编译合并到dist/css中
+import style from './components/comment/css/comment.less';
+import style2 from './components/hello/css/hello.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBoxComponent from './components/comment/js/CommentBoxComponent';
+import HelloComponent from './components/hello/js/HelloComponent';
+
+import DateComponent from './components/date/js/DateComponent';
+
+var commentData = [
+    {author: "张三1", time: "20150721", text: "张三说好"},
+    {author: "李四1", time: "20150721", text: "李四说好"}
+];
+
+//ReactDOM.render(
+//<CommentBoxComponent commentItems={commentData} />,
+//    document.body
+//);
+
+
+ReactDOM.render(<DateComponent />, document.body);
+//
+//ReactDOM.render(
+//    <HelloComponent />,
+//    document.body
+//);
+
+
+
+
